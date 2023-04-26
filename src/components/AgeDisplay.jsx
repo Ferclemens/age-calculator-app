464ac4edd69b6d1e80d7e5c8d72c705fbca68cdb
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-04-26 10:02:50 -0300

    :sparkles: Start project

---
+++ src/components/AgeDisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AgeDisplay() {
+  return (
+    <div>AgeDisplay</div>
+  )
+}
+
+export default AgeDisplay
---
+++ src/components/DateInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function DateInput() {
+  return (
+    <section>
+        <label>DAY</label>
+        <input type='date'></input>
+        <label>MONTH</label>
+        <input type='date'></input>
+        <label>YEAR</label>
+        <input type='date'></input>
+    </section>
+  )
+}
+
+export default DateInput
